c9a89d9d47411a57af3a38936405a91d5060865e
Gary Siu <Gary@untypeable.com>
2015-08-13 18:01:15 +0100

    Added first event listener to basic calc and tested button working

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var basicNum1 = document.getElementById('basic-num-1').value;
+var basicNum2 = document.getElementById('basic-num-2').value;
+var basicOp = document.getElementById('basic-operation').value;
+var butBasicCalc = document.getElementById('basic-calc');
+butBasicCalc.addEventListener('click', function() {
+  console.log('You clicked the button!')
+});
