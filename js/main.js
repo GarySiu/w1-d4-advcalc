67e7965dd8537d520d26d0e8bbed17f4f0075dea
Gary Siu <Gary@untypeable.com>
2015-08-13 18:31:19 +0100

    Add comments for clarity

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
+// Add an event listener to the calculate button on the basic calculator
 var butBasicCalc = document.getElementById('basic-calc');
 butBasicCalc.addEventListener('click', function() {
-  var basicNum1 = document.getElementById('basic-num-1').value;
-  var basicNum2 = document.getElementById('basic-num-2').value;
+// Anonymous function that assigns the 2 numbers and operator to variables on click
+  var basicNum1 = parseFloat(document.getElementById('basic-num-1').value);
+  var basicNum2 = parseFloat(document.getElementById('basic-num-2').value);
   var basicOp = document.getElementById('basic-operation').value;
+// Basic calculator logic
+
+// Put the answer in the answer area and then reveal it
+  document.getElementById('basic-answer-alert').innerHTML = basicNum1 + basicNum2;
   document.getElementById('basic-answer').setAttribute('class','show');
-  document.getElementById('basic-answer-alert').innerHTML = parseFloat(basicNum1) + parseFloat(basicNum2);
 });
