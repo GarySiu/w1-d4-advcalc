a6e61c4a23d480c1a675e8e6db489752ba1cbb82
Gary Siu <Gary@untypeable.com>
2015-08-13 18:19:33 +0100

    Basic functionality without logic handling the operator choice

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
-var basicNum1 = document.getElementById('basic-num-1').value;
-var basicNum2 = document.getElementById('basic-num-2').value;
-var basicOp = document.getElementById('basic-operation').value;
 var butBasicCalc = document.getElementById('basic-calc');
 butBasicCalc.addEventListener('click', function() {
-  console.log('You clicked the button!')
+  var basicNum1 = document.getElementById('basic-num-1').value;
+  var basicNum2 = document.getElementById('basic-num-2').value;
+  var basicOp = document.getElementById('basic-operation').value;
+  document.getElementById('basic-answer').setAttribute('class','show');
+  document.getElementById('basic-answer-alert').innerHTML = parseFloat(basicNum1) + parseFloat(basicNum2);
 });
